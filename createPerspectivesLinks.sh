abab176367f6c0c9a30675df51fcf181aeda8f2e
Joop Ringelberg <joop.ringelberg@gmail.com>
2022-04-25 16:45:36 +0200

    Added createPerspectivesLinks

---
+++ createPerspectivesLinks.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# NOTE: this script should be adapted with each new tagged version!
+
+#cd .psc-package/pv0.15.0/aff-sockets
+
+##### SPAGO #####
+cd .spago
+
+
+##### PERSPECTIVES-UTILITIES
+cd perspectives-utilities
+
+rm -Rf v v1.0.0
+ln -s ../../../perspectives-utilities v1.0.0
+
